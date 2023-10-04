a4445cedce4e626da5ff77a7f08fcfc60ba8e905
Ivan Orlov <orlov@google.com>
2023-10-04 04:32:42 +0000

    Point SlurmV5 modules to "maintenance_periodic" fork.

--- community/modules/compute/schedmd-slurm-gcp-v5-partition/main.tf
+++ community/modules/compute/schedmd-slurm-gcp-v5-partition/main.tf
@@ -38,7 +38,7 @@ data "google_compute_zones" "available" {
 }
 
 module "slurm_partition" {
-  source = "github.com/SchedMD/slurm-gcp.git//terraform/slurm_cluster/modules/slurm_partition?ref=5.9.1"
+  source = "github.com/mr0re1/slurm-gcp.git//terraform/slurm_cluster/modules/slurm_partition?ref=maintenance_periodic"
 
   slurm_cluster_name                = local.slurm_cluster_name
   partition_nodes                   = var.node_groups
